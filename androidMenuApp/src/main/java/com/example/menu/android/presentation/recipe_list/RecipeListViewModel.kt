bea47f9e3bf92b2e7877f0b48ed2372d1c14e4ca
festuskiambi <kiamfestoh@gmail.com>
2022-06-23 20:12:09 +0300

    Add RecipeList Viewmodel create recipe list view model

---
+++ androidMenuApp/src/main/java/com/example/menu/android/presentation/recipe_list/RecipeListViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.menu.android.presentation.recipe_list
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RecipeListViewModel
+@Inject
+constructor(
+    private  val savedStateHandle: SavedStateHandle
+): ViewModel() {
+}
